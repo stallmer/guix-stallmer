dfa8acd09acf014042122e4b00f3fae8b4498f97
Stephen Daunheimer <github.ripple521@aleeas.com>
2024-10-23 15:24:47 -0700

    First commit of my channel!

---
+++ packages/tailscale.scm
@@ -0,0 +1,32 @@
+(define-module (packages tailscale)
+	       #:use-module (guix packages)
+               #:use-module (gnu packages linux)
+	       #:use-module (guix gexp)
+	       #:use-module (guix build-system copy)
+	       #:use-module (guix download)
+	       #:use-module (guix licenses))
+
+(define-public tailscale
+  (package
+    (name "tailscale")
+    (version "1.76.1")
+    (source (origin
+              (method url-fetch/tarbomb)
+              (uri (string-append "https://pkgs.tailscale.com/stable/tailscale_" version
+				  "_amd64.tgz"))
+              (sha256
+                (base32
+                  "0dk0p4jc91p6c1jg944ljvanj85r3szjs6zl4xh71anq20vlj4bb"))))
+    (build-system copy-build-system)
+    (arguments
+      (list
+        #:install-plan
+        #~`((,(string-append "tailscale_" #$version "_amd64/") "/bin/"))))
+    (propagated-inputs
+      ; iptables is required for setting up routing
+      (list iptables))
+    (synopsis "Tailscale connects your team's devices and development environments for easy access to remote resources.")
+    (description
+      "Tailscale is a zero config VPN for building secure networks. Install on any device in minutes. Remote access from any network or physical location.")
+    (home-page "https://tailscale.com/")
+    (license #f)))
